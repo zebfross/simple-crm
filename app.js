b3c9cf4ed03fbe818856c884f10ca9589b0bbc99
= <zebfross@hotmail.com>
2014-12-07 22:59:33 -0700

    added post install script

--- app.js
+++ app.js
@@ -145,12 +145,3 @@ else if (env == 'test')
 var app = init(envConfig);
 module.exports = app;
 
-app.set('port', process.env.PORT || 3000);
-
-http.createServer(app).listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port'));
-});
-/*https.createServer(app).listen(443, function() {
-	console.log('Express server listening on port ' + server.address().port);
-});*/
-
