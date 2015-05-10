ff49085fffd0cf9fdd48dc86a72b8d1453fc5916
= <zebfross@hotmail.com>
2015-05-09 18:36:55 -0600

    started code to accept emails

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,30 @@ var router = express.Router();
 var passport = require('passport')
 var models = require('../models');
 var Activity = models.Activity;
+var email = require('emailjs')
+var server = email.server.connect({
+	user: "zeb+zebfross.com",
+	password: "fEz5_leARs",
+	host: "just51.justhost.com",
+	ssl: true
+})
+
+router.post("/incoming-email", function(req, res) {
+	console.log("post to /incoming-email")
+	var msg = JSON.stringify(req.body)
+	server.send({
+		text: msg,
+		from: "Zeb Fross <kingzebulon@gmail.com",
+		to: "Zeb Fross <zebfross@hotmail.com>",
+		subject: "Testing Email!"
+	}, function(err, message) {
+		if(err) {
+			throw err
+		} else {
+			res.send(message)
+		}
+	})
+});
 
 require('./auth')(router)
 
