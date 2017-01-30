049320ccdc8914391e02c97eb02b7cf7ce17e755
Zeb Fross <zebfross@hotmail.com>
2017-01-29 18:38:33 -0800

    fixed views

--- config/config.js
+++ config/config.js
@@ -94,7 +94,7 @@ module.exports = {
         db: createDbConnectionString("ds131729.mlab.com:31729", "simplecrm"),
         root: rootPath,
         notifier: notifier,
-        viewPath: '../views',
+        viewPath: 'views',
 		secret: 'ebb65a09-0f30-41db-b9ad-9a199a0db862',
         app: {
             name: 'Simple CRM'
